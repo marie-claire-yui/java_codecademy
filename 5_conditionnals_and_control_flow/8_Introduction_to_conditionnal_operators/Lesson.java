dedd8720d6584bc9293888b5d5a0068b65ce864f
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-09 16:30:45 +0200

    a

---
+++ 5_conditionnals_and_control_flow/8_Introduction_to_conditionnal_operators/Lesson.java
@@ -0,0 +1,11 @@
+// Introduction to conditional operators
+
+// Java includes operators that only use boolean values. 
+// These conditional operators help simplify expressions containing complex boolean relationships 
+//by reducing multiple boolean values to a single value: true or false.
+
+// if we want to run a code block only if multiple conditions are true. We could use the AND operator: &&.
+// we want to run a code block if at least one of two conditions are true. We could use the OR operator: ||.
+// we can produce the opposite value, where true becomes false and false becomes true, with the NOT operator: !.
+// check truth table
+
