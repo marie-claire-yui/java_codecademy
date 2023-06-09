bde8d558010cdc1a9ddf47f358f0ee1be8dac734
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-09 23:17:51 +0200

    a

---
+++ 5_conditionnals_and_control_flow/12_combining_conditional_operators/Operators.java
@@ -0,0 +1,19 @@
+// Combining Conditional Operators
+// boolean foo = true && !(false || !true)--> true
+
+public class Operators {
+  public static void main(String[] args) {
+    int a = 6;
+    int b = 3;
+
+    boolean ex1 = !(a == 7 && (b >= a || a != a));
+    System.out.println(ex1); // true
+
+    boolean ex2 = a == b || !(b > 3);
+    System.out.println(ex2); // true
+
+    boolean ex3 = !(b <= a && b != a + b);
+    System.out.println(ex3); // false
+
+  }
+}
