ef60b4149fa8062bcc78d68b136b54b0bbabf558
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-12 21:07:48 +0200

    a

---
+++ 6_array_and_arrayLists/17_review/Review.java
@@ -0,0 +1,10 @@
+public class Review{
+public static void main(String[] args) {
+    
+  }
+}
+
+//Remember: To run your code, enter the following commands in the terminal:
+//javac Review.java
+// and then
+// java Review
