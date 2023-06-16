f08073ec537191308bd47f426a8e4a8551a5d331
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-16 20:22:20 +0200

    a

---
+++ 8_String_methods/2_length()/TweetCounter.java
@@ -0,0 +1,16 @@
+// length()
+
+// the length() string method returns the length ⁠— total number of characters ⁠— of a String.
+
+public class TweetCounter {
+  
+	public static void main(String[] args) {
+    
+    String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";  
+
+    // What's the character count?
+    System.out.println(tweet.length()); //83
+    
+  }
+  
+}
