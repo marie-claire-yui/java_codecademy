243391cdad8a09f0874d2e598ab011f0a33a1776
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-16 18:50:56 +0200

    a

---
+++ 7_loops/4_For_loops/For_loops.java
@@ -0,0 +1,23 @@
+// For Loops
+
+// A for loop header is made up of the following three parts, each separated by a semicolon:
+
+//     The initialization of the loop control variable.
+//     A boolean expression.
+//     An increment or decrement statement.
+
+// In a for loop, an initialization statement is run once in order to initialize the loop control variable.
+// This variable is modified in every iteration, can be referenced in the loop body, 
+// and used to test the boolean condition. In the example above, i is the loop control variable.
+
+// for (int i = 0; i < 5; i++) {
+  
+    // i = 0: i is initialized to 0
+    // i < 5: the loop is given a boolean condition that relies on the value of i. The loop will continue to execute until i < 5 is false.
+    // i++: i will increment at the end of each loop and before the condition is re-evaluated.
+
+//   // code that will run
+    //the code will run through the loop a total of five times.
+// }
+
+// iterate = means that we are repeating the same block of code.
