723bcbb40d88c7e521d8e145102132ef02ee46c0
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-16 20:43:55 +0200

    a

---
+++ 8_String_methods/8_toUpperCase()_toLowerCase()/Example.java
@@ -0,0 +1,10 @@
+public class Example{
+    public static void main(String[] args) {
+        String input = "Cricket!";
+        String upper = input.toUpperCase();  //returns the string value converted to uppercase
+        String lower = input.toLowerCase(); // returns the string value converted to lowercase
+
+        System.out.println(upper);// stores "CRICKET!"
+        System.out.println(lower);// stores "cricket!"
+    }
+}
