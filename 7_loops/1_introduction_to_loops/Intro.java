28b9536f8ae8755e044c3c8ba3cdb7bee0c0a1ec
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-16 18:31:27 +0200

    a

---
+++ 7_loops/1_introduction_to_loops/Intro.java
@@ -0,0 +1,16 @@
+// INTRODUCTION TO LOOPS
+
+// A loop is a programming tool that allows developers to repeat the same block of code until some condition is met. 
+
+// First, a starting condition is evaluated. If the starting condition is true, then the loop body is executed.
+// When the last line of the loop body is executed, the condition is re-evaluated. 
+//This process continues until the condition is false 
+//(if the condition never becomes false, we can actually end up with an infinite loop!). 
+//If the starting condition is false, the loop never gets executed.
+
+// We employ loops to easily scale programs - saving time and minimizing mistakes.
+
+
+    // while loops
+    // for loops
+    // for-each loops
