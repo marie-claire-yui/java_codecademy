dd6cb55439749a89476d3a4329ea80b5b37dec81
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-16 19:57:37 +0200

    a

---
+++ 7_loops/10_Review/Playground.java
@@ -0,0 +1,41 @@
+//Review
+
+//while loops:
+// These are useful to repeat a code block an unknown number of times until some condition is met. 
+// int wishes = 0;
+ 
+// while (wishes < 3) {
+ 
+//   // code that will run
+//   wishes++;
+ 
+// }
+
+// for loops: 
+//These are ideal for when you are incrementing or decrementing with a counter variable.
+// for (int i = 0; i < 5; i++) {
+ 
+//   // code that will run
+ 
+// }
+
+// For-each loops:
+//  These make it simple to do something with each item in a list. 
+// for (String inventoryItem : inventoryItems) {
+ 
+//   // do something with each inventoryItem
+ 
+// }
+
+
+
+class Playground {
+
+	public static void main(String[] args) {
+  
+    for (int i = 0; i < 28; i++) {
+  		System.out.println("Congrats on finishing Java loops!");
+	  }
+  }
+
+}
