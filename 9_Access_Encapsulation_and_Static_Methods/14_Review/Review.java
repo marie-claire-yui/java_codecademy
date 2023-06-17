562387a2c80f221d02f65b6bac17ddd4550647b4
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-17 15:28:31 +0200

    a

---
+++ 9_Access_Encapsulation_and_Static_Methods/14_Review/Review.java
@@ -0,0 +1,11 @@
+public class Review{
+    public static void main(String[] args) {
+        
+    }
+}
+
+// To review, here are some of the main takeaways about static methods and variables:
+//     Static methods and variables are associated with the class as a whole, not objects of the class.
+//     Static methods and variables are declared as static by using the static keyword upon declaration.
+//     Static methods cannot interact with non-static instance variables. This is due to static methods not having a this reference.
+//     Both static methods and non-static methods can interact with static variables.
