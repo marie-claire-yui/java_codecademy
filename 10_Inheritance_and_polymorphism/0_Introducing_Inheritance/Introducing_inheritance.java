910d56d385fa0f55a9bf2f5b370d644286b093bf
marie-claire-yui <marieclairekasai@gmail.com>
2023-06-17 16:07:12 +0200

    a

---
+++ 10_Inheritance_and_polymorphism/0_Introducing_Inheritance/Introducing_inheritance.java
@@ -0,0 +1,18 @@
+// Introducing Inheritance
+
+// important feature of POO
+//Java class can also inherit traits from another class.
+
+// Example: parent class shape and child class triangle
+//Because a Triangle is a Shape, we can define Triangle so that it inherits fields and methods directly from Shape.
+// A reference of type Shape can refer to an object of Shape or an object of Triangle.
+//Our Triangle class will inherit all the traits of Shape,
+// but Triangle can also contain its own unique methods and variables.
+//For example, we could have an instance variable called hypotenuse and a method called findHypotenuse() 
+//that can only be accessed by Triangle class references.
+// Objects of Triangle can call any method contained in Triangle or Shape
+
+// There are several terms you’ll encounter frequently:
+
+//     Parent class, superclass, and base class refer to the class that another class inherits from (like Shape).
+//     Child class, subclass, and derived class refer to a class that inherits from another class (like Triangle).
